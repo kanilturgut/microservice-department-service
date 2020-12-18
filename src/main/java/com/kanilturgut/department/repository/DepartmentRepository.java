38ccb4ca2fa6126694640c26bcfc6d36b56797dc
Kadir Anıl Turğut <kanilturgut@gmail.com>
2020-12-18 23:34:16 +0300

    initial

---
+++ src/main/java/com/kanilturgut/department/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package com.kanilturgut.department.repository;
+
+import com.kanilturgut.department.entitiy.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Department findByDepartmentId(Long departmentId);
+}
